9a7e8b0224f8da5c3384600cb68a7e58368deee5
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-22 23:46:11 +0700

    membuat function MahasiswaFormView dan parameter untuk button

--- app/src/main/java/com/example/meet8/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/meet8/ui/view/screen/MahasiswaFormView.kt
@@ -1,2 +1,9 @@
 package com.example.meet8.ui.view.screen
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MahasiswaFormView(
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){}
