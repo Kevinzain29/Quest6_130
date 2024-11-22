3223fa95cf2ce8e06274860fe4ce10ea214d4bc6
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-22 23:39:21 +0700

    membuat function SplashView dan menambahkan Column

--- app/src/main/java/com/example/meet8/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/meet8/ui/view/screen/SplashView.kt
@@ -1,2 +1,21 @@
 package com.example.meet8.ui.view.screen
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashView(
+    onMulaiButton: ()->Unit,
+){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
