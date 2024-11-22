eed219fb29706a32fa7f56a86e1a74c971b6abdf
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-22 23:32:25 +0700

    membuat object MataKuliah yang didalamnya terdapat listof/daftar nama-nama MK

--- app/src/main/java/com/example/meet8/ui/data/MataKuliah.kt
+++ app/src/main/java/com/example/meet8/ui/data/MataKuliah.kt
@@ -1,2 +1,12 @@
 package com.example.meet8.ui.data
 
+object MataKuliah {
+    val options = listOf(
+        "Pengembangan mobile lanjut",
+        "pengembangan aplikasi web frame work",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
+}
