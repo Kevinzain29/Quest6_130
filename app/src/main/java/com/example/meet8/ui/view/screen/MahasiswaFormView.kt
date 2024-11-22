368a34322f03738eccd92212a68aae8b40390acc
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-22 23:50:46 +0700

    menambahkan Column dan didalamnya (Row, Image, Columm dan Text)

--- app/src/main/java/com/example/meet8/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/meet8/ui/view/screen/MahasiswaFormView.kt
@@ -12,5 +12,37 @@ fun MahasiswaFormView(
     var email by remember { mutableStateOf("") }
 
     val listData: MutableList<String> = mutableListOf(nama,nim,email)
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(
+                id = R.color.primary
+            )
+            ), horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.umy
+                ),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(75.dp)
+                    .padding(bottom = 15.dp)
+            )
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Red, fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Unggul dan Islami",
+                    color = Color.Red,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
+    }
 }
