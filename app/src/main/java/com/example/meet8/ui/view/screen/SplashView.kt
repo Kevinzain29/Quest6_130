95288f757a19c131fbe6a55f1b8dcf353eb9394b
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-22 23:43:31 +0700

    menambahkan Button 'mulai' dan mengimport fungsi-fungsi

--- app/src/main/java/com/example/meet8/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/meet8/ui/view/screen/SplashView.kt
@@ -1,6 +1,24 @@
 package com.example.meet8.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.meet8.R
 
 @Composable
 fun SplashView(
@@ -24,5 +42,14 @@ fun SplashView(
             contentDescription = "",
             modifier = Modifier.size(200.dp)
         )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = {onMulaiButton()},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(30.dp)
+        ) {
+            Text(text = "mulai")
+        }
     }
 }
