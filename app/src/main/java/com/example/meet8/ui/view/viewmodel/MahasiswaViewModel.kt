89bebb2f5a535bd2586b71233355d66c7ddcb0e5
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-23 00:01:23 +0700

    menambahkan class MahasiswaViewModel dan function saveDataMahasiswa untuk menyimpan data mahasiswa

--- app/src/main/java/com/example/meet8/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/meet8/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,3 +1,25 @@
 package com.example.meet8.ui.view.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.meet8.ui.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
+class MahasiswaViewModel : ViewModel() {
+    private val _mahasiswaUiState = MutableStateFlow(Mahasiswa())
+
+    //response
+    val mahasiswaUiState: StateFlow<Mahasiswa> = _mahasiswaUiState.asStateFlow()
+
+    fun saveDataMahasiswa(ls: MutableList<String>) {
+        _mahasiswaUiState.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                nim = ls[1],
+                email = ls[2],
+            )
+        }
+    }
+}
