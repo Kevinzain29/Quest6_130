263ea867678a99dc6efc0ef55422ee95ade17513
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-11-22 23:57:02 +0700

    menambahkan function MataKuliahView

--- app/src/main/java/com/example/meet8/ui/view/screen/MataKuliahView.kt
+++ app/src/main/java/com/example/meet8/ui/view/screen/MataKuliahView.kt
@@ -1,2 +1,12 @@
 package com.example.meet8.ui.view.screen
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MataKuliahView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+
+}
